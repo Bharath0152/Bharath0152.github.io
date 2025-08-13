ed5f67c5e5bbaf4a7d306ba4e3c09b09e5fcd627
Bharath0152 <bharathpc4@gmail.com>
2025-08-13 11:48:21 +0530

    Update splash.js

--- splash.js
+++ splash.js
@@ -2,16 +2,12 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    console.log("Splash screen script loaded. Waiting 5 seconds...");
-
     // setTimeout executes a function after a specified number of milliseconds
     setTimeout(() => {
-        
-        console.log("5 seconds have passed. Redirecting now to main.html");
         
         // This line changes the browser's URL to 'main.html'
         window.location.href = 'main.html';
 
-    }, 5000); // 5000 milliseconds = 5 seconds
+    }, 4000); // 4000 milliseconds = 4 seconds
 
 });
